c73630d117b3217d955fffeeb4abeb6b4748defe
emmanouil emmanouil <emmanouil.e25@gmail.com>
2024-05-15 11:59:58 +0100

    remove double property

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -86,7 +86,6 @@ export const login = async (
       domain: DOMAIN,
       path: "/",
       expires: expirationTime,
-      expires: expirationTime,
       httpOnly: true,
     });
     res.status(200).json({
